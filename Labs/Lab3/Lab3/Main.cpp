483f74603b2fb5b8ccf47841e069d49cf1dd5270
aleksandr <hersgory2134@gmail.com>
2022-11-30 22:21:57 +0300

    fix

--- Labs/Lab3/Lab3/Main.cpp
+++ Labs/Lab3/Lab3/Main.cpp
@@ -3,19 +3,33 @@
 //этой   дороге.С   помощью  алгоритма  Дейкстры   определить
 //максимальный   груз, который  можно  провести  между   двумя
 //указанными городам(10).
-/*
- Предлагаю хранить матрицу в 2х мерном массиве
-*/
 
-#include "MaxWeightFinder.h"
+#include <fstream>
+#include <iostream>
+#include <string>
 #include <vector>
 
 using namespace std;
 
 int main() {
-
+	ifstream in("\\in.txt"); // Open file to Read   
+	
 	vector<vector<int>> matrix;
 	
+	int x, y, weight = 0;
+
+	
+	cin >> x >> y >> weight;
+
+	cout << x << " - " << y << " " << weight << endl;
+
+
+	vector<bool> isVisited(matrix.size(), false);
+
+
+	
+
+
 
 	return 1;
 }

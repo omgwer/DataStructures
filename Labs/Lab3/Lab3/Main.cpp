3d66cffaefba2de95f84789ecb8207f67d0b5bb9
aleksandr <hersgory2134@gmail.com>
2022-12-01 20:47:53 +0300

    feat: add input in file func

--- Labs/Lab3/Lab3/Main.cpp
+++ Labs/Lab3/Lab3/Main.cpp
@@ -11,25 +11,46 @@
 
 using namespace std;
 
-int main() {
-	ifstream in("\\in.txt"); // Open file to Read   
+int searchMax() {
+	ifstream searchMax("in.txt");
+	int result = 0, max = 0;
+	if (searchMax.is_open()) {
+		while (!searchMax.eof()) {  
+			searchMax >> max;
+			if (max > result) {
+				result = max;
+			}
+			searchMax >> max;
+			if (max > result) {
+				result = max;
+			}
+			searchMax >> max;
+		}
+	}
+	searchMax.close();
+	return result;
+}
+
+
+int main() {	
+	vector<vector<int>> matrix(searchMax(), vector<int>(searchMax(), 0));
+	vector<bool> isVisited(searchMax(), false);
+	int x = 0, y = 0, weight = 0, startPoint, finishPoint;
+	ifstream in("in.txt");
+	if (in.is_open()) {
+		while (!in.eof()) {  //init array
+			in >> x >> y >> weight;
+			matrix[x][y] = weight;
+			matrix[y][x] = weight;
+		}
+	}	
+	cout << "Input start Point - ";
+	cin >> startPoint;
+	cout << endl;
+	cout << "Input finish Point - ";
+	cin >> finishPoint;
+	cout << endl;
 	
-	vector<vector<int>> matrix;
-	
-	int x, y, weight = 0;
-
-	
-	cin >> x >> y >> weight;
-
-	cout << x << " - " << y << " " << weight << endl;
-
-
-	vector<bool> isVisited(matrix.size(), false);
-
-
-	
-
-
-
+		
 	return 1;
 }

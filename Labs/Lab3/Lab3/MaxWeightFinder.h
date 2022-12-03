e98a35cba15743b20df6edc05649e5a1bd304b38
aleksandr <hersgory2134@gmail.com>
2022-12-03 19:19:54 +0300

    feat: add lab3

--- Labs/Lab3/Lab3/MaxWeightFinder.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <stdlib.h>
-
-class MaxWeightFinder
-{
-
-};
---
+++ Labs/Lab4/Lab4/Main.cpp
@@ -0,0 +1,19 @@
+//11. Реализовать   эвристический   алгоритм  решения  задачи
+//коммивояжера на основании метода  Прима  нахождения  остовного
+//дерева.Проиллюстрировать по шагам этапы поиска(11).
+
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main() {
+	vector<vector<int>> matrix;
+	vector<vector<int>> newMatrix;
+	vector<int> isVisited;
+
+
+	return 1;
+}
